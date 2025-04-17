1aa3216a3a13130157183d121a089de9a3956fe5
Riya Gor <riyaupadhyay.2201@gmail.com>
2025-04-17 17:34:48 -0400

    .

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,14 +9,13 @@ const authenticateUser  = (req, res, next) => {
     }
 
     const token = authHeader.split(" ")[1];
-
-    console.log('🔑 Received Token:', token);
-    console.log('🔐 Secret used:', process.env.JWT_SECRET);
-    
+  
     try {
         // Verify token using the secret key
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+       
         req.user = decoded; // attach user data to request
+       
         next(); // move to the next middleware/controller
     } catch (err) {
         res.status(401).json({ message: "Invalid or expired token" });
