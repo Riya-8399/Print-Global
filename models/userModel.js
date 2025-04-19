5791f78761c9afb357db7eaa5d8bc64dfb98fad0
Riya Gor <riyaupadhyay.2201@gmail.com>
2025-04-19 01:23:06 -0400

    .

--- models/userModel.js
+++ models/userModel.js
@@ -4,21 +4,30 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Name is required"],
         trim: true
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required"],
         unique: true,
         lowercase: true
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "Password is required"],
         minlength: 6
-    }
+    },
+    resetPasswordToken: {
+        type: String,
+      },
+    resetPasswordExpires: {
+        type: Date,
+      },
+      
 });
+   
+  
 
 // 🔐 Middleware: Runs before saving a user
 userSchema.pre('save', async function (next) {
